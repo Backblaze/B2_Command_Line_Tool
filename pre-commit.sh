21c05dec1745a9661d5c0e55d495b1ddad917585
Brian Beach <coder@beachfamily.net>
2016-01-31 09:46:28 -1000

    pre-commit.sh now displays diffs when updating the formatting of the code, and script won't quit in the middle if code needs updating.
    Also, added friendly messages when check pass, so it's not just silent.

--- pre-commit.sh
+++ pre-commit.sh
@@ -1,4 +1,47 @@
-#!/bin/bash -eux
-yapf --in-place b2
-pyflakes b2
-time python test_b2_command_line.py ./b2 $(head -n 1 ~/.b2_auth) $(tail -n 1 ~/.b2_auth)
+#!/bin/bash -u
+
+function header 
+{
+    echo
+    echo "#"
+    echo "# $*"
+    echo "#"
+    echo
+}
+
+header Checking Formatting
+
+if yapf b2 > b2.yapf.out
+then
+    echo "Formatting is good."
+    rm b2.yapf.out
+else
+    echo "Formatting updated:"
+    echo
+    diff b2 b2.yapf.out
+    mv b2.yapf.out b2
+    chmod +x b2
+fi
+
+header Pyflakes
+
+if pyflakes b2
+then
+    echo "Pyflakes passed"
+else
+    echo
+    echo "Pyflakes FAILED"
+    exit 1
+fi    
+
+header Tests
+
+if time python test_b2_command_line.py ./b2 $(head -n 1 ~/.b2_auth) $(tail -n 1 ~/.b2_auth)
+then
+    echo "Tests passed"
+else
+    echo
+    echo "Tests FAILED"
+    exit 1
+fi
+
