ae9ddcae684c577493e9949706bf0d8ed447be43
Brian Beach <coder@beachfamily.net>
2016-03-07 14:34:50 -1000

    Minimal framework for running unit tests.
    Travis CI will now run all of the tests in the test directory.

---
+++ test/test_sync.py
@@ -0,0 +1,17 @@
+######################################################################
+#
+# File: b2/test_sync.py
+#
+# Copyright 2016 Backblaze Inc. All Rights Reserved.
+#
+# License https://www.backblaze.com/using_b2_code.html
+#
+######################################################################
+
+import unittest
+
+
+class TestSync(unittest.TestCase):
+
+    def test_sync_down(self):
+        self.assertEqual(2, 2)
