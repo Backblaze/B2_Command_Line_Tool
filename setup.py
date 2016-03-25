1b063f1320aebf4b3bedb8256a03af58e19246df
Brian Beach <coder@beachfamily.net>
2016-03-25 08:13:42 -1000

    Bump version number for dev.

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.4.8',
+    version='0.4.9',
 
     description='Command Line Tool for Backblaze B2',
     long_description=long_description,
