136376d99a4032bd1d265b4e5d72d6b046feb44f
Brian Beach <coder@beachfamily.net>
2016-03-31 11:13:37 -1000

    Updated sync reporting that doesn't use tqdm, and gives more info.

--- b2/sync_report.py
+++ b2/sync_report.py
@@ -11,10 +11,9 @@
 from __future__ import print_function
 
 import six
+import sys
 import threading
-
-from tqdm import tqdm
-
+import time
 
 class SyncReport(object):
     """
@@ -37,19 +36,22 @@ class SyncReport(object):
     STATE_DONE = 'done'
 
     def __init__(self):
+        self.start_time = time.time()
         self.state = 'local'
         self.local_file_count = 0
         self.compare_count = 0
+        self.total_transfer_files = 0  # set in end_compare()
         self.total_transfer_bytes = 0  # set in end_compare()
+        self.transfer_files = 0
         self.transfer_bytes = 0
-        self.tqdm = self._make_tqdm()
+        self.current_line = ''
         self.lock = threading.Lock()
+        self._update_progress()
 
     def close(self):
         with self.lock:
             if self.state != self.STATE_DONE:
-                self._remove_tqdm()
-                self.state = self.STATE_DONE
+                self._print_line('', False)
 
     def print_completion(self, message):
         """
@@ -57,9 +59,58 @@ class SyncReport(object):
         bar back.
         """
         with self.lock:
-            self._remove_tqdm()
-            print(message)
-            self.tqdm = self._make_tqdm()
+            self._print_line(message, True)
+            self._update_progress()
+
+    def _update_progress(self):
+        rate = int(self.transfer_bytes / (time.time() - self.start_time))
+        if self.state == self.STATE_LOCAL:
+            message = ' count: %d files   compare: %d files   transferred: %d files   %d bytes   %d B/s' % (
+                self.local_file_count,
+                self.compare_count,
+                self.transfer_files,
+                self.transfer_bytes,
+                rate
+            )
+        elif self.state == self.STATE_COMPARE:
+            message = ' compare: %d/%d files   transferred: %d files   %d bytes   %d B/s' % (
+                self.compare_count,
+                self.local_file_count,
+                self.transfer_files,
+                self.transfer_bytes,
+                rate
+            )
+        elif self.state == self.STATE_TRANSFER:
+            message = ' compare: %d/%d files   transferred: %d/%d files   %d/%d bytes   %d B/s' % (
+                self.compare_count,
+                self.local_file_count,
+                self.transfer_files,
+                self.total_transfer_files,
+                self.transfer_bytes,
+                self.total_transfer_bytes,
+                rate
+            )
+        else:
+            message = ''
+        self._print_line(message, False)
+
+    def _print_line(self, line, newline):
+        """
+        Prints a line to stdout.
+
+        :param line: A string without a \r or \n in it.
+        :param newline: True if the output should move to a new line after this one.
+        """
+        if len(line) < len(self.current_line):
+            line += ' ' * (len(self.current_line) - len(line))
+        sys.stdout.write(line)
+        if newline:
+            sys.stdout.write('\n')
+            self.current_line = ''
+        else:
+            sys.stdout.write('\r')
+            self.current_line = line
+        sys.stdout.flush()
 
     def update_local(self, delta):
         """
@@ -68,17 +119,16 @@ class SyncReport(object):
         with self.lock:
             assert self.state == self.STATE_LOCAL
             self.local_file_count += delta
-            self.tqdm.update(delta)
+            self._update_progress()
 
     def end_local(self):
         """
         Local file count is done.  Can proceed to step 2.
         """
         with self.lock:
-            self._remove_tqdm()
             assert self.state == self.STATE_LOCAL
             self.state = self.STATE_COMPARE
-            self.tqdm = self._make_tqdm()
+            self._update_progress()
 
     def update_compare(self, delta):
         """
@@ -86,78 +136,25 @@ class SyncReport(object):
         """
         with self.lock:
             self.compare_count += delta
-            if self.state == self.STATE_COMPARE:
-                self.tqdm.update(delta)
+            self._update_progress()
 
-    def end_compare(self, total_transfer_bytes):
+    def end_compare(self, total_transfer_files, total_transfer_bytes):
         with self.lock:
-            self._remove_tqdm()
             assert self.state == self.STATE_COMPARE
             self.state = self.STATE_TRANSFER
-            self.tqdm = self._make_tqdm()
+            self.total_transfer_files = total_transfer_files
             self.total_transfer_bytes = total_transfer_bytes
+            self._update_progress()
 
-    def update_transfer(self, delta):
+    def update_transfer(self, file_delta, byte_delta):
         with self.lock:
-            self.transfer_bytes += delta
-            if self.state == self.STATE_TRANSFER:
-                self.tqdm.update(delta)
+            self.transfer_files += file_delta
+            self.transfer_bytes += byte_delta
+            self._update_progress()
 
-    def _add_tqdm(self):
-        """
-        Creates a progress bar and displays it.
-        """
-        assert self.tqdm is None
-        tqdm = self._make_tqdm()
-        if tqdm is not None:
-            self.tqdm = tqdm
-            self.tqdm.__enter__()
-            self.tqdm.update(0)
 
-    def _remove_tqdm(self):
-        """
-        Gets rid of any progress bar that is being displayed.
-        """
-        if self.tqdm is not None:
-            self.tqdm.__exit__(None, None, None)
-            self.tqdm = None
-
-    def _make_tqdm(self):
-        """
-        Makes a progress bar if we're in a state that needs one.
-
-        The formats do not include speed.  We keep re-creating the bar,
-        so speed computation doesn't work.
-        """
-        if self.state == self.STATE_LOCAL:
-            return tqdm(
-                total=123456,
-                desc='Step 1/3: count local files',
-                unit='file',
-                bar_format='{desc} {n_fmt}',
-                initial=self.local_file_count
-            )
-        elif self.state == self.STATE_COMPARE:
-            return tqdm(
-                total=self.local_file_count,
-                desc='Step 2/3: compare file lists',
-                unit='file',
-                bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}',
-                initial=self.compare_count
-            )
-        elif self.state == self.STATE_TRANSFER:
-            return tqdm(
-                total=self.total_transfer_bytes,
-                desc='Step 3/3: transfer data',
-                unit='B',
-                unit_scale=True,
-                bar_format='{l_bar}{bar}| {n_fmt}/{total_fmt}',
-                initial=self.transfer_bytes
-            )
-
-def sample_run():
-    import time
-    sync_report = SyncReport()
+def sample_run(report_class):
+    sync_report = report_class()
 
     for i in six.moves.range(20):
         sync_report.update_local(1)
@@ -174,18 +171,19 @@ def sample_run():
         if i == 3:
             sync_report.print_completion('transferred: b.txt')
         if i == 4:
-            sync_report.update_transfer(25)
-    sync_report.end_compare(50)
+            sync_report.update_transfer(25, 25000)
+    sync_report.end_compare(50, 50000)
 
     for i in six.moves.range(25):
         if i % 2 == 0:
             sync_report.print_completion('transferred: %d.txt' % i)
-        sync_report.update_transfer(1)
+        sync_report.update_transfer(1, 1000)
         time.sleep(0.2)
 
     sync_report.close()
 
-sample_run()
+
+sample_run(SyncReport)
 
 
 
