f0474c359b7a8fb412ddb3fd6d88641b3ab5fddf
Brian Beach <coder@beachfamily.net>
2016-04-07 13:42:45 -1000

    Version 0.5.2

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.5.1',
+    version='0.5.2',
 
     description='Command Line Tool for Backblaze B2',
     long_description=long_description,
