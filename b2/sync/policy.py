5185223236430760910157e844ccfa251fed48be
Pawel Polewicz <p.polewicz@gmail.com>
2016-07-28 02:59:41 +0200

    Put files_are_different back in sync.policy

--- b2/sync/policy.py
+++ b2/sync/policy.py
@@ -12,7 +12,7 @@ from abc import ABCMeta, abstractmethod
 
 import six
 
-from .utils import files_are_different
+from ..exception import CommandError, DestFileNewer
 from .action import LocalDeleteAction, B2DeleteAction, B2DownloadAction, B2HideAction, B2UploadAction
 
 ONE_DAY_IN_MS = 24 * 60 * 60 * 1000
@@ -43,9 +43,51 @@ class AbstractFileSyncPolicy(object):
             return True
         else:
             # Both exist.  Transfer only if the two are different.
-            return files_are_different(
-                self._source_file, self._dest_file, self._args
-            )  # TODO: don't pass args here?
+            return self.files_are_different(self._source_file, self._dest_file, self._args)
+
+    @classmethod
+    def files_are_different(cls, source_file, dest_file, args):
+        """
+        Compare two files and determine if the the destination file
+        should be replaced by the source file.
+        """
+
+        # Compare using modification time by default
+        compareVersions = args.compareVersions or 'modTime'
+
+        # Compare using file name only
+        if compareVersions == 'none':
+            return False
+
+        # Compare using modification time
+        elif compareVersions == 'modTime':
+            # Get the modification time of the latest versions
+            source_mod_time = source_file.latest_version().mod_time
+            dest_mod_time = dest_file.latest_version().mod_time
+
+            # Source is newer
+            if dest_mod_time < source_mod_time:
+                return True
+
+            # Source is older
+            elif source_mod_time < dest_mod_time:
+                if args.replaceNewer:
+                    return True
+                elif args.skipNewer:
+                    return False
+                else:
+                    raise DestFileNewer(dest_file.name,)
+
+        # Compare using file size
+        elif compareVersions == 'size':
+            # Get file size of the latest versions
+            source_size = source_file.latest_version().size
+            dest_size = dest_file.latest_version().size
+
+            # Replace if sizes are different
+            return source_size != dest_size
+        else:
+            raise CommandError('Invalid option for --compareVersions')
 
     def get_all_actions(self):
         if self._should_transfer():
