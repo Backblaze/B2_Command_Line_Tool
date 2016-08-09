Backblaze/B2_Command_Line_Tool

commit 17eedfac49e688cd3a80b86850df38377366c1bc

Catch timeouts thrown by requests libary
Resolves #206

b2/b2http.py
@@ -17,7 +17,7 @@ import requests
 import six
 import time
 
-from .exception import B2Error, BrokenPipe, B2ConnectionError, interpret_b2_error, UnknownError, UnknownHost
+from .exception import B2Error, BrokenPipe, B2ConnectionError, B2ConnectionTimeout, B2ReadTimeout, interpret_b2_error, UnknownError, UnknownHost
 from .version import USER_AGENT
 from six.moves import range
 
@@ -70,6 +70,12 @@ def _translate_errors(fcn, post_params=None):
                     # code.
                     raise BrokenPipe()
         raise B2ConnectionError(str(e0))
+
+    except requests.ConnectTimeout as e:
+        raise B2ConnectionTimeout(str(e))
+
+    except requests.ReadTimeout as e:
+        raise B2ReadTimeout(str(e))
 
     except Exception as e:
         # Don't expect this to happen.

b2/exception.py
@@ -112,6 +112,36 @@ class DestFileNewer(B2Error):
         return True
 
 
+class B2ReadTimeout(B2Error):
+    def __init__(self, message):
+        super(B2ConnectionTimeout, self).__init__()
+        self.message = message
+
+    def should_retry_http(self):
+        return True
+
+    def should_retry_upload(self):
+        return True
+
+    def __str__(self):
+        return 'Connection timeout: %s' % (self.message,)
+
+
+class B2ConnectionTimeout(B2Error):
+    def __init__(self, message):
+        super(B2ConnectionTimeout, self).__init__()
+        self.message = message
+
+    def should_retry_http(self):
+        return True
+
+    def should_retry_upload(self):
+        return True
+
+    def __str__(self):
+        return 'Connection timeout: %s' % (self.message,)
+
+
 class DuplicateBucketName(B2Error):
     def __init__(self, bucket_name):
         super(DuplicateBucketName, self).__init__()

commit 96f8f9cab26d02989d646f8bb6f1af56ddbc545c

Restore compatibility with requests<2.11.0
it was broken accidentally in 17eedfac49e688cd3a80b86850df38377366c1bc

b2/b2http.py
@@ -17,7 +17,7 @@ import requests
 import six
 import time
 
-from .exception import B2Error, BrokenPipe, B2ConnectionError, B2ConnectionTimeout, B2ReadTimeout, interpret_b2_error, UnknownError, UnknownHost
+from .exception import B2Error, BrokenPipe, B2ConnectionError, B2RequestTimeout, interpret_b2_error, UnknownError, UnknownHost
 from .version import USER_AGENT
 from six.moves import range
 
@@ -71,11 +71,8 @@ def _translate_errors(fcn, post_params=None):
                     raise BrokenPipe()
         raise B2ConnectionError(str(e0))
 
-    except requests.ConnectTimeout as e:
-        raise B2ConnectionTimeout(str(e))
-
-    except requests.ReadTimeout as e:
-        raise B2ReadTimeout(str(e))
+    except requests.Timeout as e:
+        raise B2RequestTimeout(str(e))
 
     except Exception as e:
         # Don't expect this to happen.

b2/exception.py
@@ -115,11 +115,7 @@ class B2ConnectionError(TransientErrorMixin, B2SimpleError):
     pass
 
 
-class B2ConnectionTimeout(TransientErrorMixin, B2SimpleError):
-    pass
-
-
-class B2ReadTimeout(TransientErrorMixin, B2SimpleError):
+class B2RequestTimeout(TransientErrorMixin, B2SimpleError):
     pass
 
 