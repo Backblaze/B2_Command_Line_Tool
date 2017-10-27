11c72b7cab6d8335ad5032a322d856818539b599
Alex Tsui <alextsui05@gmail.com>
2017-10-26 18:37:00 -0700

    Fix formatting with yapf

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -16,10 +16,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('../b2'))
-
 
 # -- General configuration ------------------------------------------------
 
@@ -30,10 +30,9 @@ sys.path.insert(0, os.path.abspath('../b2'))
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc',
-    'sphinx.ext.intersphinx',
-    'sphinx.ext.ifconfig',
-    'sphinx.ext.viewcode']
+extensions = [
+    'sphinx.ext.autodoc', 'sphinx.ext.intersphinx', 'sphinx.ext.ifconfig', 'sphinx.ext.viewcode'
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -79,7 +78,6 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = False
 
-
 # -- Options for HTML output ----------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
@@ -113,12 +111,10 @@ html_sidebars = {
     ]
 }
 
-
 # -- Options for HTMLHelp output ------------------------------------------
 
 # Output file base name for HTML help builder.
 htmlhelp_basename = 'B2_Command_Line_Tooldoc'
-
 
 # -- Options for LaTeX output ---------------------------------------------
 
@@ -144,20 +140,19 @@ latex_elements = {
 # (source start file, target name, title,
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
-    (master_doc, 'B2_Command_Line_Tool.tex', u'B2\\_Command\\_Line\\_Tool Documentation',
-     u'Backblaze', 'manual'),
+    (
+        master_doc, 'B2_Command_Line_Tool.tex', u'B2\\_Command\\_Line\\_Tool Documentation',
+        u'Backblaze', 'manual'
+    ),
 ]
-
 
 # -- Options for manual page output ---------------------------------------
 
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    (master_doc, 'b2_command_line_tool', u'B2_Command_Line_Tool Documentation',
-     [author], 1)
+    (master_doc, 'b2_command_line_tool', u'B2_Command_Line_Tool Documentation', [author], 1)
 ]
-
 
 # -- Options for Texinfo output -------------------------------------------
 
@@ -165,13 +160,11 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-    (master_doc, 'B2_Command_Line_Tool', u'B2_Command_Line_Tool Documentation',
-     author, 'B2_Command_Line_Tool', 'One line description of project.',
-     'Miscellaneous'),
+    (
+        master_doc, 'B2_Command_Line_Tool', u'B2_Command_Line_Tool Documentation', author,
+        'B2_Command_Line_Tool', 'One line description of project.', 'Miscellaneous'
+    ),
 ]
-
-
-
 
 # Example configuration for intersphinx: refer to the Python standard library.
 intersphinx_mapping = {'https://docs.python.org/': None}
