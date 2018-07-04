d1044677aceb6db7b28fc34bf00c3994ce9c2699
Brian Beach <coder@beachfamily.net>
2018-07-04 13:26:14 -1000

    It turns out that 3.7 was release just a week ago, and the test environments aren't there yet.

--- setup.py
+++ setup.py
@@ -98,7 +98,6 @@ setup(
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
     ],
 
     # What does your project relate to?
