dd338c070b22b3d21a24391c6c91985ed3652bf7
Pawel Polewicz <p.polewicz@gmail.com>
2018-07-22 03:14:20 +0200

    Fix StubProgressListener when used as context manager

--- test/test_bucket.py
+++ test/test_bucket.py
@@ -65,12 +65,6 @@ class StubProgressListener(AbstractProgressListener):
     def close(self):
         self.history.append('closed')
 
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
-
 
 class CanRetry(B2Error):
     """
