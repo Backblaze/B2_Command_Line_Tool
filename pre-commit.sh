c5374311a2269d95720420b560f51e0d964356e7
Dmitry Paramonov <dmitry.paramonov@reef.pl>
2019-02-13 18:32:55 +0300

    removed tests for the raw API

--- pre-commit.sh
+++ pre-commit.sh
@@ -109,16 +109,6 @@ do
     fi
 done
 
-header test_raw_api
-
-if TEST_ACCOUNT_ID="$(head -n 1 ~/.b2_auth)" TEST_APPLICATION_KEY="$(tail -n 1 ~/.b2_auth)" python -m b2.__main__ test_raw_api
-then
-    echo "raw API test PASSED"
-else
-    echo "raw API test FAILED"
-    exit 1
-fi
-
 if [[ $# -ne 0 && "${1:-}" == quick ]]
 then
     header QUICK
