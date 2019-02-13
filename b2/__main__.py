29dcc2ddb439b828da83775ced955137c4696498
Dmitry Paramonov <dmitry.paramonov@reef.pl>
2019-02-13 18:32:30 +0300

    __main__.py added

---
+++ b2/__main__.py
@@ -0,0 +1,15 @@
+######################################################################
+#
+# File: __main__.py
+#
+# Copyright 2018 Backblaze Inc. All Rights Reserved.
+#
+# License https://www.backblaze.com/using_b2_code.html
+#
+######################################################################
+
+from __future__ import absolute_import
+
+from .console_tool import main
+
+main()
