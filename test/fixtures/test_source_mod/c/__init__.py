5841a7cdfb9d58bacf58eff17eae76b914869f7f
Dmitry Paramonov <dmitry.paramonov@reef.pl>
2019-02-21 18:30:49 +0300

    New data added to fixtures

---
+++ test/fixtures/test_source_mod/c/__init__.py
@@ -0,0 +1,13 @@
+######################################################################
+#
+# File: __init__.py
+#
+# Copyright 2018 Backblaze Inc. All Rights Reserved.
+#
+# License https://www.backblaze.com/using_b2_code.html
+#
+######################################################################
+
+
+def func1(x):
+    return x + 1
--- test/fixtures/test_target_mod/c/__init__.py
+++ test/fixtures/test_target_mod/c/__init__.py
@@ -1,2 +1,17 @@
+######################################################################
+#
+# File: __init__.py
+#
+# Copyright 2018 Backblaze Inc. All Rights Reserved.
+#
+# License https://www.backblaze.com/using_b2_code.html
+#
+######################################################################
+
+
+def func2(x):
+    return x + 2
+
+
 def z(x):
     return x + 10
