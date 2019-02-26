50fd6bf7f4ca1241cedb9992ee25b4035aadab21
Dmitry Paramonov <dmitry.paramonov@reef.pl>
2019-02-26 17:05:09 +0300

    Wildcards added to excluded names

--- setup.py
+++ setup.py
@@ -104,7 +104,7 @@ setup(
 
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
-    packages=find_packages(exclude=['contrib', 'docs', 'test']),
+    packages=find_packages(exclude=['*contrib*', '*docs*', '*test*']),
 
     # Alternatively, if you want to distribute just a my_module.py, uncomment
     # this:
