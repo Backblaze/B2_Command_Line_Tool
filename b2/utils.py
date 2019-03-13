4cddb65a4061d9d4df37083aa6195c6055cee412
Dmitry Paramonov <dmitry.paramonov@reef.pl>
2019-03-13 11:32:52 +0300

    Add utility function to represent dictionaries in a deterministic way

--- b2/utils.py
+++ b2/utils.py
@@ -9,6 +9,7 @@
 ######################################################################
 
 import sys
+import six
 import time
 import warnings
 import b2sdk.utils
@@ -28,6 +29,22 @@ def current_time_millis():
     File times are in integer milliseconds, to avoid roundoff errors.
     """
     return int(round(time.time() * 1000))
+
+
+def repr_dict_deterministically(dict_):
+    """
+    Represent a dictionary in a deterministic way, i.e. with
+    the same order of keys
+
+    :param dict_: a dictionary
+    :type dict_: dict
+    :return: a string representation of a dictionary
+    :rtype: str
+    """
+    # a simple version had a disadvantage of outputting dictionary keys in random order.
+    # It was hard to read. Therefore we sort items by key.
+    fields = ', '.join('%s: %s' % (repr(k), repr(v)) for k, v in sorted(six.iteritems(dict_)))
+    return '{%s}' % (fields,)
 
 
 def _show_warning(source_name, target_name, attr_name):
