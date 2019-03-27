26cb5d11eea44e9661c1164ece2099b73b000d2a
Pawel Polewicz <p.polewicz@gmail.com>
2019-03-27 15:34:50 +0100

    Fix copyright dates

--- b2/__main__.py
+++ b2/__main__.py
@@ -2,7 +2,7 @@
 #
 # File: __main__.py
 #
-# Copyright 2018 Backblaze Inc. All Rights Reserved.
+# Copyright 2019 Backblaze Inc. All Rights Reserved.
 #
 # License https://www.backblaze.com/using_b2_code.html
 #
