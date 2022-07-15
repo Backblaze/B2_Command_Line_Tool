395cad63fcc642f7f78a7c07ef60d57728c9cebf
Pawel Polewicz <p.polewicz@gmail.com>
2022-07-15 13:30:53 +0200

    Increase verbosity for integration tests

--- noxfile.py
+++ noxfile.py
@@ -156,7 +156,7 @@ def integration(session):
     """Run integration tests."""
     install_myself(session)
     session.install(*REQUIREMENTS_TEST)
-    session.run('pytest', '-s', '-x', '-n', '4', *session.posargs, 'test/integration')
+    session.run('pytest', '-s', '-x', '-v', '-n', '4', *session.posargs, 'test/integration')
 
 
 @nox.session(python=PYTHON_VERSIONS)
