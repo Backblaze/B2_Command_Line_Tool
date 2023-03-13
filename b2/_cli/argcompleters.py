a5c9c81d5ebea4658910713e0f273a19aecbc23d
Maciej Urbanski <maciej.urbanski+gh@reef.pl>
2023-03-13 21:52:18 +0100

    use cache in bucket_name_completer if supported

--- b2/_cli/argcompleters.py
+++ b2/_cli/argcompleters.py
@@ -7,6 +7,7 @@
 # License https://www.backblaze.com/using_b2_code.html
 #
 ######################################################################
+import inspect
 from functools import wraps
 from itertools import islice
 
@@ -29,7 +30,9 @@ def _with_api(func):
 
 @_with_api
 def bucket_name_completer(api: B2Api, **kwargs):
-    return [bucket.name for bucket in api.list_buckets()]
+    supports_cache = "cache" in inspect.signature(api.list_buckets).parameters
+    list_kwargs = {"cache": True} if supports_cache else {}
+    return [bucket.name for bucket in api.list_buckets(**list_kwargs)]
 
 
 @_with_api
