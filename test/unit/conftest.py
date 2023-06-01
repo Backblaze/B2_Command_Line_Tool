350d4f8bd3f4da0da8dcd5cefb685feb9118d54a
Maciej Urbanski <maciej.urbanski+gh@reef.pl>
2023-06-01 16:11:02 +0200

    add License header fixer

--- test/unit/conftest.py
+++ test/unit/conftest.py
@@ -1,3 +1,12 @@
+######################################################################
+#
+# File: test/unit/conftest.py
+#
+# Copyright 2023 Backblaze Inc. All Rights Reserved.
+#
+# License https://www.backblaze.com/using_b2_code.html
+#
+######################################################################
 from unittest import mock
 
 import pytest
