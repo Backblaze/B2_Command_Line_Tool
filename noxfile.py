3652ee839ca8e8dbbe4d540f8180decaa51c7f95
Michał Nowacki <michal.nowacki@reef.pl>
2023-07-21 15:22:37 +0200

    pushing docker test

--- noxfile.py
+++ noxfile.py
@@ -33,7 +33,7 @@ PYTHON_VERSIONS = [
     '3.10',
     '3.11',
 ] if NOX_PYTHONS is None else NOX_PYTHONS.split(',')
-PYTHON_DEFAULT_VERSION = PYTHON_VERSIONS[-1]
+PYTHON_DEFAULT_VERSION = PYTHON_VERSIONS[-2]
 PYTHON_VERSIONS += ['3.12']  # move this into PYTHON_VERSION above after official 3.12 release
 
 PY_PATHS = ['b2', 'test', 'noxfile.py', 'setup.py']
