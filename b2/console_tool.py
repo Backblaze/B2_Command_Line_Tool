0bb4449c5878e2fbef3d8d1cccbe5ae9cb1755d8
Henry Wilkinson <henry@wilkinson.graphics>
2023-08-11 20:06:06 -0400

    Update b2/console_tool.py
    Co-authored-by: Paweł Polewicz <p.polewicz@gmail.com>

--- b2/console_tool.py
+++ b2/console_tool.py
@@ -2546,7 +2546,7 @@ class UpdateBucket(DefaultSseMixin, Command):
         parser.add_argument('--bucketInfo', type=json.loads)
         parser.add_argument(
             '--corsRules',
-            type=json.loads
+            type=json.loads,
             help=
             "If given, the bucket will have a 'custom' CORS configuration. Accepts a JSON string."
         )
