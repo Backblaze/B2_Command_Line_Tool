540757917536646fb48ac56fea94b62e65ab8353
Vykintas Baltrušaitis <vykintas.baltrusaitis@reef.pl>
2023-11-20 09:35:28 +0200

    Fix type hints: import annotations form future

--- b2/console_tool.py
+++ b2/console_tool.py
@@ -9,6 +9,7 @@
 # License https://www.backblaze.com/using_b2_code.html
 #
 ######################################################################
+from __future__ import annotations
 
 import argparse
 import base64
