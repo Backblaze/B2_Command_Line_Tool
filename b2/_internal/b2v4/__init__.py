38dde2d2013e62362db52bca70ca97a3d7210b43
Adal Chiriliuc <adal.chiriliuc@reef.pl>
2024-05-01 20:27:21 +0300

    renamed _b2v4 to b2v4, b2 points to b2v4

--- b2/_internal/b2v4/__init__.py
+++ b2/_internal/b2v4/__init__.py
@@ -1,6 +1,6 @@
 ######################################################################
 #
-# File: b2/_internal/_b2v4/__init__.py
+# File: b2/_internal/b2v4/__init__.py
 #
 # Copyright 2023 Backblaze Inc. All Rights Reserved.
 #
